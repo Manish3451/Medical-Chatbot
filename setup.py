4f0f84c09792171a76da083f481fa8043b151c7e
Manish3451 <manishmandal10@hotmail.com>
2025-09-07 20:48:19 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setup import find_packages,setup
+
+setup (
+    name = "medical chatbot",
+    version = "0.1.0",
+    author="Manish Mandal",
+    author_email = "manishmandal192@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
